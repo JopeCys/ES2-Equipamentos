8d74d1202e63c2331f49ceef20facd69ce3c5bc0
chibeg <pa.barreto2301@gmail.com>
2025-05-29 10:57:21 -0300

    Hello World endpoint

---
+++ src/main/java/es2/microsservico/exemplo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package es2.microsservico.exemplo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+
+}
