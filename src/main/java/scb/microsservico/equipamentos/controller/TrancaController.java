05422ff0e684fee3c69477c165a314afd9afec28
João Pedro Cysneiros <167272228+JopeCys@users.noreply.github.com>
2025-06-05 14:52:15 -0300

    Backup

---
+++ src/main/java/scb/microsservico/equipamentos/controller/TrancaController.java
@@ -0,0 +1,10 @@
+package scb.microsservico.equipamentos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TrancaController 
+{
+    @GetMapping("tranca/{idTranca}")
+    public
+
+}
