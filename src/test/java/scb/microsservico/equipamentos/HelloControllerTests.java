2252792976527f6a3f86d77fbf2b88737861928c
João Pedro Cysneiros <167272228+JopeCys@users.noreply.github.com>
2025-06-05 15:02:18 -0300

    Implementação do HelloControllerTests

---
+++ src/test/java/scb/microsservico/equipamentos/HelloControllerTests.java
@@ -0,0 +1,25 @@
+package scb.microsservico.equipamentos;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import scb.microsservico.equipamentos.controller.HelloController;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloWorld() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+}
